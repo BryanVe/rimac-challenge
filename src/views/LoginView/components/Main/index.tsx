b5fe611dc35b1cc6a541c7dc7972d4f5507c9778
Bryan Vera <bryan.ve.bv@gmail.com>
2024-09-12 03:08:36 -0500

    feat: :sparkles: add landing image and form header to Login view

--- src/views/LoginView/components/Main/index.tsx
+++ src/views/LoginView/components/Main/index.tsx
@@ -1,3 +1,5 @@
+import { rimacLanding } from '~/assets/images'
+
 import { Sparkles } from './components'
 
 import './style.scss'
@@ -5,6 +7,28 @@ import './style.scss'
 const Main = () => {
 	return (
 		<main className='login-section'>
+			<div className='login-section__content'>
+				<div className='login-section__landing-image'>
+					<img
+						draggable={false}
+						alt='Portada de RIMAC Seguros'
+						src={rimacLanding}
+					/>
+				</div>
+				<div className='login-section__form'>
+					<div className='tag-promo'>Seguro Salud Flexible</div>
+					<div>
+						<h1 className='login-section__form-title'>
+							Creado para ti y tu familia
+						</h1>
+						<div className='login-section__form-divider' />
+					</div>
+					<p className='login-section__form-description'>
+						Tú eliges cuánto pagar. Ingresa tus datos, cotiza y recibe nuestra
+						asesoría. 100% online.
+					</p>
+				</div>
+			</div>
 			<Sparkles />
 		</main>
 	)
