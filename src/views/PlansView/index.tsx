803ac3afc17b718fd7dffce698253c8cf29be808
Bryan Vera <bryan.ve.bv@gmail.com>
2024-09-13 20:44:02 -0500

    feat: :sparkles: add title and description to PlansView

--- src/views/PlansView/index.tsx
+++ src/views/PlansView/index.tsx
@@ -1,5 +1,38 @@
+import { Text, Title } from '~/components'
+
+import './style.scss'
+
 const PlansView = () => {
-	return <div>PlansView</div>
+	return (
+		<div className='plans-view'>
+			<Title
+				order={2}
+				size={{
+					desktop: 40,
+					mobile: 28
+				}}
+				lh={{
+					desktop: 48,
+					mobile: 36
+				}}
+				ls={{
+					desktop: -0.6,
+					mobile: -0.8
+				}}
+				className='plans-view__title'
+			>
+				Rocío ¿Para quién deseas cotizar?
+			</Title>
+			<Text
+				size={16}
+				lh={28}
+				ls={0.1}
+				className='plans-view__description'
+			>
+				Selecciona la opción que se ajuste más a tus necesidades.
+			</Text>
+		</div>
+	)
 }
 
 export default PlansView
